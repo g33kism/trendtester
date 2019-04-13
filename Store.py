3703ba547011017a2f005e980a22ff2eb3876749
g33kism <gunrajsm@gmail.com>
2019-04-13 17:10:33 -0400

    Correcting session management configuration in Flask

--- Store.py
+++ Store.py
@@ -1,17 +1,14 @@
 class dataStore:
         dataList=[]
-        graphs=dict()
         symbolList=[]
         optionList=[]
-         
+
         def __init__(self):
                 self.dataList=[]
-                self.graphs=dict()
                 self.symbolList=[]
                 self.optionList=[]
-        
+
         def reset(self):
                 self.dataList=[]
-                self.graphs=dict()
                 self.symbolList=[]
                 self.optionList=[]
--- app.py
+++ app.py
@@ -8,21 +8,35 @@ import plotly
 import plotly.graph_objs as go
 from flask import Flask, jsonify, render_template, request,session
 from pandas_datareader import data as pdr
+from datetime import timedelta
 from Store import dataStore
+from flask_session import Session
+
 
 app = Flask(__name__)
 app.secret_key = 'render_templateqweaqdfsafASDAS,smaCount=smaCount)'
-store= dataStore()
+app.config['SESSION_PERMANENT'] = True
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=5)
+
+# The maximum number of items the session stores
+# before it starts deleting some, default 500
+app.config['SESSION_FILE_THRESHOLD'] = 100
+Session(app)
+
 @app.route('/')
 def main():
     session['smaCount']=0
-    store.reset()
+    session['smas']=[]
+    session['hasBolinger']=False
+    session['store']=dataStore()
     return render_template('index.html')
 
 @app.route('/rule')
 def rule():
+    store=session['store']
     return render_template('rule.html',options=store.optionList)
-    
+
 @app.route('/sma')
 def sma():
     if 'smaCount' in session:
@@ -31,67 +45,50 @@ def sma():
          smaCount = 0
     smaCount=smaCount+1;
     session['smaCount']=smaCount
-    return render_template('sma.html',smaCount=smaCount)   
+    return render_template('sma.html',smaCount=smaCount)
 
 @app.route('/plotSMAs',methods=['POST'])
 def plotSMAs():
+    store=session['store']
     smas=request.form.getlist('sma[]')
-    print smas
-    graphs=store.graphs
+    session['smas']=smas
     dataList=store.dataList
-    graphData=graphs.get("data")
     for sma in smas:
         counter=0
         for data in dataList:
             data[store.symbolList[counter]+"_SMA_"+sma]=pd.rolling_mean(data.Close,int(sma))
-            graphData.append(go.Scatter(
-                    x=data.index,  # Can use the pandas data structures directly
-                    y=data[store.symbolList[counter]+"_SMA_"+sma],
-                    name=store.symbolList[counter]+"_SMA_"+sma
-                )
-           )
             store.optionList.append(store.symbolList[counter]+"_SMA_"+sma)
             counter=counter+1
-    graphs["data"]=graphData
-    store.graphs=graphs
     store.dataList=dataList
+    graphs=plotGraphs(store)
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    session['store']=store
     return graphJSON
 
 
 @app.route('/plotBoll',methods=['POST'])
 def plotBoll():
-    graphs=store.graphs
+    store=session['store']
+    print(session['store'])
     dataList=store.dataList
-    graphData=graphs.get("data")
     counter=0
+    session['hasBolinger'] = True
     for data in dataList:
         data[store.symbolList[counter]+'_Bol_upper'] = pd.rolling_mean(data.Close, window=20) + 2* pd.rolling_std(data.Close, 20, min_periods=20)
         data[store.symbolList[counter]+'_Bol_lower'] = pd.rolling_mean(data.Close, window=20) - 2* pd.rolling_std(data.Close, 20, min_periods=20)
-        graphData.append(go.Scatter(
-                x=data.index,  # Can use the pandas data structures directly
-                y=data[store.symbolList[counter]+"_Bol_upper"],
-                name=store.symbolList[counter]+"_Bol_upper"
-            )
-        )
         store.optionList.append(store.symbolList[counter]+"_Bol_upper")
-        graphData.append(go.Scatter(
-                x=data.index,  # Can use the pandas data structures directly
-                y=data[store.symbolList[counter]+"_Bol_lower"],
-                name=store.symbolList[counter]+"_Bol_lower"
-            )
-        )
         store.optionList.append( store.symbolList[counter]+"_Bol_lower")
         counter=counter+1
-    graphs["data"]=graphData
-    store.graphs=graphs
     store.dataList=dataList
+    graphs=plotGraphs(store)
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    session['store']=store
     return graphJSON
 
 @app.route('/getSymbols',methods=['POST'])
 def getSymbols():
     session['smaCount']=0
+    store=dataStore()
     store.reset()
     _symbols=request.form['symbols']
     _from=datetime.datetime.strptime(request.form['from'], '%m/%d/%Y').strftime('%Y-%m-%d')
@@ -99,7 +96,20 @@ def getSymbols():
     symbolList= _symbols.split(',');
     store.symbolList=symbolList
     dataList=[];
-    graphs=dict(
+
+    for symbol in symbolList:
+        store.optionList.append(symbol+"_Close")
+        data =pdr.get_data_yahoo(symbol,start=_from, end=_to)
+        dataList.append(data)
+    #session['data']=dataList
+    store.dataList=dataList
+    graphs=plotGraphs(store)
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    #
+    session['store']=store
+    return graphJSON
+def getGraphDict(_symbols):
+    return dict(
         data=[],
         layout=dict(
             title=_symbols,
@@ -111,27 +121,9 @@ def getSymbols():
             )
         )
     )
-    graphData=graphs.get("data")
-    for symbol in symbolList:
-        store.optionList.append(symbol+"_Close")
-        data =pdr.get_data_yahoo(symbol,start=_from, end=_to)
-        dataList.append(data)
-        graphData.append(go.Scatter(
-                    x=data.index,  # Can use the pandas data structures directly
-                    y=data.Close,
-                    name=symbol
-                )
-           )
-    graphs["data"]=graphData
 
-  
-    #session['data']=dataList
-    store.dataList=dataList
-    store.graphs=graphs
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    return graphJSON
-    
 def executeRule(ruleL,ruleRule,ruleSignal,ruleR,data):
+    store=session['store']
     data['signal']=""
     data['price']=0.0
     data.fillna(0)
@@ -159,10 +151,10 @@ def executeRule(ruleL,ruleRule,ruleSignal,ruleR,data):
                 data['price'][i]=data[store.symbolList[0]+'_Open'][i]
         elif data['signal'][i]=='BUY':
             buyCount+=1
-            data['price'][i]=-1*data[store.symbolList[0]+'_Open'][i] 
+            data['price'][i]=-1*data[store.symbolList[0]+'_Open'][i]
         else:
             data['price'][i]=0
-        totalProfit+=data['price'][i] 
+        totalProfit+=data['price'][i]
         if totalProfit>maxProfit:
             maxProfit=totalProfit
             peakdays=0
@@ -177,6 +169,7 @@ def executeRule(ruleL,ruleRule,ruleSignal,ruleR,data):
 
 @app.route('/runRules',methods=['POST'])
 def runRules():
+    store=session['store']
     ruleL=request.form.getlist('ruleL[]')
     ruleRule=request.form.getlist('ruleRule[]')
     ruleSignal=request.form.getlist('ruleSignal[]')
@@ -191,11 +184,45 @@ def runRules():
         'Low':store.symbolList[counter]+'_Low','Close':store.symbolList[counter]+'_Close','Adj Close':store.symbolList[counter]+'_Adj Close',
         'Volume':store.symbolList[counter]+'_Volume'}, inplace=True)
         resultData=pd.concat([resultData, data], axis=1, join_axes=[data.index])
-        counter=counter+1             
+        counter=counter+1
     #result = pd.concat([df1, df4], axis=1, join_axes=[df1.index])
     results=executeRule(ruleL,ruleRule,ruleSignal,ruleR,resultData)
+    session['store']=store
     return render_template('result.html',totalProfit=results[0],profit=results[1],loss=results[2],
-    drawdown=results[3],peak=results[4],buy=results[5],sell=results[6])   
+    drawdown=results[3],peak=results[4],buy=results[5],sell=results[6])
+
+def appendGraphData(graphData,data,column,isClose):
+    if isClose == True:
+        graphData.append(go.Scatter(
+            x=data.index,  # Can use the pandas data structures directly
+            y=data.Close,
+            name=column
+            )
+        )
+    else:
+        graphData.append(go.Scatter(
+            x=data.index,  # Can use the pandas data structures directly
+            y=data[column],
+            name=column
+            )
+        )
+
+def plotGraphs(store):
+     graphs = getGraphDict(','.join(store.symbolList))
+     graphData=graphs.get("data")
+     counter=0
+     for data in store.dataList:
+         appendGraphData(graphData,data,store.symbolList[counter],True)
+         if session['hasBolinger']:
+            appendGraphData(graphData,data,store.symbolList[counter]+"_Bol_upper",False)
+            appendGraphData(graphData,data,store.symbolList[counter]+"_Bol_lower",False)
+
+         if 'smas' in session:
+             for sma in session['smas']:
+                 appendGraphData(graphData,data,store.symbolList[counter]+"_SMA_"+sma,False)
+         counter=counter+1
+     graphs["data"]=graphData
+     return graphs
 
 
 if __name__=='__main__':
